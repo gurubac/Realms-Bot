6ed60839f2d0bedce703dc9c82470a7a2f31d5a9
gurubac <gbrbachchu@gmail.com>
2023-06-30 11:26:03 -0700

    fix login command

--- commands/realm/login.js
+++ commands/realm/login.js
@@ -1,55 +1,68 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { Authflow } = require('prismarine-auth');
 const { RealmAPI } = require('prismarine-realms');
 const fs = require('node:fs');
 const userIdentifier = 'any unique identifier';
 const cacheDir = './logincache';
-const authflow = new Authflow(userIdentifier, cacheDir, undefined, (res) => {
-  console.log('Intermediate token:', res);
-});
 
 function isJsonNotEmpty(content) {
-    try {
-      const parsedJson = JSON.parse(content);
-      return Array.isArray(parsedJson) ? parsedJson.length > 0 : Object.keys(parsedJson).length > 0;
-    } catch (error) {
-      return false;
-    }
+  try {
+    const parsedJson = JSON.parse(content);
+    return Array.isArray(parsedJson) ? parsedJson.length > 0 : Object.keys(parsedJson).length > 0;
+  } catch (error) {
+    return false;
   }
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('login')
     .setDescription('Login mc.'),
 
-  async execute(interaction) {
+  execute(interaction) {
     try {
-        const cacheExists = fs.existsSync(cacheDir);
-        if (cacheExists) {
-            const cacheFiles = fs.readdirSync(cacheDir);
-            let isValidCache = false;
+      const cacheExists = fs.existsSync(cacheDir);
+      if (cacheExists) {
+        const cacheFiles = fs.readdirSync(cacheDir);
+        let isValidCache = false;
 
-            for (const file of cacheFiles) {
-            const filePath = `${cacheDir}/${file}`;
-            const fileContent = fs.readFileSync(filePath, 'utf-8');
+        for (const file of cacheFiles) {
+          const filePath = `${cacheDir}/${file}`;
+          const fileContent = fs.readFileSync(filePath, 'utf-8');
 
-            if (isJsonNotEmpty(fileContent)) {
-                isValidCache = true;
-                break;
-            }
-            }
-
-            if (isValidCache) {
-            return interaction.reply('You are already logged in.');
-            }
+          if (isJsonNotEmpty(fileContent)) {
+            isValidCache = true;
+            break;
+          }
         }
 
+        if (isValidCache) {
+          return interaction.reply('You are already logged in.');
+        }
+      }
 
-      // Prompt the user to login through the bot
-      await authflow.getMsaToken();
+      // Create a Promise to resolve with the intermediate token
+      const getTokenPromise = new Promise((resolve) => {
+        const authflow = new Authflow(userIdentifier, cacheDir, undefined, (res) => {
+          // console.log('Intermediate response:', res);
+          resolve(res);
+        });
+        authflow.getMsaToken();
+      });
 
-      // Send the login message and code to the user
-      await interaction.reply("hello!");
+      // Wait for the promise to resolve and obtain the intermediate token
+      getTokenPromise.then((userCodeResponse) => {
+        const embed = new EmbedBuilder()
+          .setTitle('Login Information')
+          .addFields(
+            { name: 'Instructions', value: userCodeResponse.message, inline: true },
+            { name: 'Login Code', value: userCodeResponse.userCode, inline: true },
+          )
+        return interaction.reply({
+          embeds: [embed],
+        });
+      });
+
     } catch (error) {
       console.error(error);
       return interaction.reply('An error occurred while logging in.');
