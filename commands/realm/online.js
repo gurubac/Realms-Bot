bfb5435a9aae2748d143d8c8c657f346b2709f3f
copbrick <anthonyjbenjamin@gmail.com>
2023-07-04 15:24:09 -0700

    fix merge conflic on online.js

--- commands/realm/online.js
+++ commands/realm/online.js
@@ -12,7 +12,6 @@ const authflow = new Authflow(config.userIdentifier, config.cacheDir);
 const api = RealmAPI.from(authflow, "java");
 
 module.exports = {
-<<<<<<< HEAD
   data: new SlashCommandBuilder()
     .setName("online")
     .setDescription("Shows who is currently online on the realm.")
@@ -48,8 +47,7 @@ module.exports = {
           "You are not logged in. Please run the `/login` command."
         );
       }
-      const realm = await api.getRealm(realmId);
-      const players = realm.players;
+      const {players} = await api.getRealm(realmId);
 
       // Filter online players
       const onlinePlayers = players.filter((player) => player.online);
@@ -67,39 +65,6 @@ module.exports = {
         embed.addFields({ name: "Player List", value: playerList });
       } else {
         embed.addFields({ name: "Player List", value: "No players online." });
-=======
-    data: new SlashCommandBuilder()
-      .setName('online')
-      .setDescription('Shows who is currently online on the realm.'),
-  
-    async execute(interaction) {
-      try {
-        if (!isLoggedIn(config.cacheDir)) {
-          return interaction.reply('You are not logged in. Please run the `/login` command.');
-        }
-        const {players} = await api.getRealm(realmId);
-  
-        // Filter online players
-        const onlinePlayers = players.filter((player) => player.online);
-  
-        // Create an embed
-        const embed = new EmbedBuilder()
-          .setTitle('Online Players')
-          .setDescription(`Total Online: ${onlinePlayers.length}`);
-  
-        // Add player list field
-        if (onlinePlayers.length > 0) {
-          const playerList = onlinePlayers.map((player) => player.name).join('\n');
-          embed.addFields({ name: 'Player List', value: playerList });
-        } else {
-          embed.addFields({ name: 'Player List', value: 'No players online.' });
-        }
-  
-        return interaction.reply({ embeds: [embed] });
-      } catch (error) {
-        console.error(error);
-        return interaction.reply('An error occurred while generating the player list.');
->>>>>>> da9de4572bb85944eb5bd70b45823d75e267c9fe
       }
 
       return interaction.reply({ embeds: [embed] });
