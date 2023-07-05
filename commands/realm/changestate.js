7e157410402f479711d6854d5783dcea1b7fd986
copbrick <anthonyjbenjamin@gmail.com>
2023-07-04 17:38:17 -0700

    Revert "fixed error handling changestate"
    This reverts commit c1fdbb3c062a80fc6197c8e7763955f0861dcd55.

--- commands/realm/changestate.js
+++ commands/realm/changestate.js
@@ -12,12 +12,12 @@ const api = RealmAPI.from(authflow, "java");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("changestate")
-		.setDescription("Change state of your realm to `open` / `close`")
-		.addStringOption((option) =>
-			option.setName("id")
-				.setDescription("ID of your Realm")
-				.setRequired(true)
-		),
+        .setDescription("Change state of your realm to `open` / `close`")
+        .addStringOption((option) => 
+            option.setName("id")
+                .setDescription("ID of your Realm")
+                .setRequired(true)
+        ),
 
 	async execute(interaction) {
 		try {
@@ -26,26 +26,22 @@ module.exports = {
 					"You are not logged in. Please run the `/login` command."
 				);
 			}
+            
 
 
-
-			const realmID = await interaction.options.getString("id");
-
-			try {
-				const { state } = await api.getRealm(realmID);
-				/*
-				Flag that acts as a switch depending on the server's state.
-				We change the state based on the current status.
-				*/
-				const newState = state === "OPEN" ? "close" : "open";
-				await api.changeRealmState(realmID, newState);
-
-				interaction.reply(`Server state changed to ${newState}`);
-			} catch (error) {
-				console.error(error);
-				return interaction.reply("You have insufficient permissions to change the state of this Realm. Ensure you are the owner.");
-			}
-
+            const realmID = await interaction.options.getString("id");
+            
+            const { state } = await api.getRealm(realmID);
+            
+            /*
+            Flag that acts as a switch depending on the server's state.
+            We change the state based on the current status.
+            Acts as a boolean switch.
+            */
+            const newState = state === "OPEN" ? "close" : "open";
+            await api.changeRealmState(realmID, newState);
+            
+            interaction.reply(`Server state changed to ${newState}`);
 
 			//needs to be changed to support multiple realms
 			// const { downloadUrl } = await api.getRealmWorldDownload(`${config.realmId}`, "1");
@@ -54,7 +50,7 @@ module.exports = {
 			// return interaction.reply({ embeds: [embed] });			
 		} catch (error) {
 			console.error(error);
-			return interaction.reply("An error has occurred while executing this command. Please check logs.");
+            interaction.reply("You have insufficient permissions to change the state of this Realm. Ensure you are the owner.");
 
 		}
 	},
