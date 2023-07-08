defac716d029b3e1cf80d9821b83689658c23968
copbrick <anthonyjbenjamin@gmail.com>
2023-07-08 00:36:51 -0700

    latest

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const db = require("./src/database/Database");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
--- src/database/Database.js
+++ src/database/Database.js
@@ -13,10 +13,14 @@ const sequelize = new Sequelize({
     // schema: process.DATABASE_SCHEMA
 });
 
+
 try {
     sequelize.authenticate().then(() => {
-        console.log('Connection has been established successfully.')
+        console.log('Connection has been established successfully.');
+
     });
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
+
+module.exports = sequelize;
